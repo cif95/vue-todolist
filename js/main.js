35ec7c8fa8121d85a1bd9ff4665d49bd70e3157a
cif95 <vivianacifarelli9@gmail.com>
2022-03-18 15:56:42 +0100

    style changes

--- js/main.js
+++ js/main.js
@@ -12,7 +12,6 @@
 // 1- oltre al click sul pulsante, intercettare anche il tasto ENTER per aggiungere il todo alla lista
 // 2- cliccando sul testo dell'item, invertire il valore della proprietà done del todo corrispondente (se done era uguale a false, impostare true e viceversa)
 
-
 const app = new Vue({
 	el:'#app',
 	data: {
@@ -44,7 +43,6 @@ const app = new Vue({
 		]
 	},
 	methods: {
-
 		addTodo(text){
 			if (!text.trim() == ''){
 				const newTodo = {
